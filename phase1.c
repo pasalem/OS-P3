a4178ddd063cb46b6ad1eb346346d23cc0b00fe7
alec12094 <abenson@wpi.edu>
2015-02-11 19:48:29 -0500

    Added state field

--- phase1.c
+++ phase1.c
@@ -1,61 +1,90 @@
 #include "phase1.h"
 void * init_thread(void *arg);
-void add_job(job* rootJob, pthread_t thread, int index, char *class);
+void add_job(job* rootJob, job jobToAdd);
 void print_jobs(job *rootJob);
 void recycle_top_job(job *rootJob);
+job create_job(int id, int class, pthread_t thread);
 
 int main (int argc, char *argv[]){
    pthread_t unclassified, secret, top_secret;
    job *rootJob = malloc(sizeof(job));
+   job emptyJob;
+   emptyJob.class = EMPTY;
 
    //Seed our rand() function 
    srand(time(NULL));
 
    //Make our threads
    int index;
+   job jobToAdd;
    for(index = 0; index < NUM_THREADS; index++){
       if(index >= 0 && index < 8){
          //printf("Making unclassified thread %d\n", index);
          pthread_create(&unclassified, NULL, init_thread, NULL);
-         add_job(rootJob, unclassified, index, "U");
+         jobToAdd = create_job(index, U, unclassified); 
+         add_job(rootJob, jobToAdd);
       } else if(index >= 8 && index < 14){
          //printf("Making secret thread %d\n", index);
          pthread_create(&secret, NULL, init_thread, NULL);
-         add_job(rootJob, unclassified, index, "S");
+         jobToAdd = create_job(index, S, secret); 
+         add_job(rootJob, jobToAdd);
       } else{
          //printf("Making top secret thread %d\n", index);
-         pthread_create(&top_secret, NULL, init_thread, NULL);
-         add_job(rootJob, unclassified, index, "TS");
+         pthread_create(&top_secret, NULL, init_thread, NULL); 
+         jobToAdd = create_job(index, TS, top_secret); 
+         add_job(rootJob, jobToAdd);
       }
+   }
+
+   //Initialize the cluster
+   job* cluster = (job *)malloc(sizeof(job) * 2);
+   cluster[0] = cluster[1] = emptyJob;
+   while(TRUE){
+      //If there is an open spot in the cluster
+      if(cluster[0].class == EMPTY && cluster[1].class == EMPTY){
+
+
+      }
+
    }
 
    print_jobs(rootJob);
    pthread_exit(NULL);
 }
 
+//Initializes a new job with the given params
+job create_job(int id, int class, pthread_t thread){
+   job *newJob = (job *)malloc(sizeof(job));
+   newJob -> id = id;
+   newJob -> class = class;
+   newJob -> thread = thread;
+   newJob -> state = READY;
+   return *newJob;
+}
+
 //Moves the job at the top of the queue back to the bottom
+//TODO: change so that this does not immedietly add the item back to the bottom of the queue
 void recycle_top_job(job* rootJob){
       job *jobPointer = rootJob->next;
       //Remove the top element from the queue
       rootJob -> next = (jobPointer -> next);
-      add_job(rootJob, jobPointer->thread, jobPointer->id, jobPointer->class);
+      add_job(rootJob, *jobPointer);
       printf("Moving job %d from top of the list to the bottom\n", jobPointer->id);
       return;
-
-   printf("Failed to move job from top to bottom\n");
 }
 
 //Add a thread to the queue
-void add_job(job* rootJob, pthread_t thread, int index, char *class){
+void add_job(job* rootJob, job jobToAdd){
    job *newJob = (job *)malloc(sizeof(job));
    if( newJob == NULL){
       printf(RESET "Not enough memory to allocate for this job, aborting");
       exit(1);
    }
 
-   newJob -> class = class;
-   newJob -> id = index;
-   newJob -> thread = thread;
+   newJob -> class = jobToAdd.class;
+   newJob -> id = jobToAdd.id;
+   newJob -> thread = jobToAdd.thread;
+   newJob -> state = READY;
    newJob -> next = NULL;
 
    if(rootJob -> next == NULL){
@@ -79,14 +108,14 @@ void print_jobs(job* rootJob){
    }
    while(jobPointer -> next != NULL){
       jobPointer = jobPointer->next;
-      printf(KCYN "Thread with class %s and ID %d\n" RESET, jobPointer->class, jobPointer->id);
+      printf(KCYN "Thread with class %d and ID %d\n" RESET, jobPointer->class, jobPointer->id);
    }
 }
 
 //Runs the job for a random amount of time
 void *run_job(job *runJob) {
    int runTime = (rand() % 1750000) + 250000;
-   printf("Thread %d with status %s will run for %d microsecs", runJob->id, runJob->class, runTime);
+   printf("Thread %d with status %d will run for %d microsecs", runJob->id, runJob->class, runTime);
    usleep( runTime );
    pthread_yield();
 }
